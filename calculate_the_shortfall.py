01d7e8d697b694f98c70a826475b5b74ae17f669
haeri <hadi43@naver.com>
2023-01-10 11:59:23 +0900

    이 놀이기구의 원래 이용료는 price원 인데, 놀이기구를 N 번 째 이용한다면 원래 이용료의 N배를 받기로 하였습니다. 놀이기구를 count번 타게 되면 현재 자신이 가지고 있는 금액에서 얼마가 모자라는지를 return 하도록 solution 함수를 완성하세요. 단, 금액이 부족하지 않으면 0을 return 하세요.

--- calculate_the_shortfall.py
+++ calculate_the_shortfall.py
@@ -1,13 +1,10 @@
 def solution(price, money, count):
-    ran = range(1,count+1)
     sum = 0
-    for i in ran:
+    for i in range(1,count+1):
         total = i*price
-        sum = sum + total
+        sum += total
     if money<sum:
         answer = sum-money
         return answer
     else:
         return 0
-
-
