7c400c33ba3e3fdbda34159a9a8e8425f70c4d41
haeri <hadi43@naver.com>
2023-07-10 15:59:57 +0900

    230710(Mon)

---
+++ programmers/basic/StartingWithNthElement.py
@@ -0,0 +1,7 @@
+# 정수 리스트 num_list와 정수 n이 주어질 때, n 번째 원소부터 마지막 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+def solution(num_list, n):
+    answer = []
+    for i in range(n,len(num_list)+1):
+        answer.append(num_list[i-1])
+    return answer
